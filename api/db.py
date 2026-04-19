fb957038582c0af6f0b737619b4c51cb9f138c33
karolina <kkomorowskaa@gmail.com>
2026-04-19 20:04:24 +0200

    app

--- api/db.py
+++ api/db.py
@@ -1,1 +1,18 @@
-# TODO
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+# To są dane logowania do Twojej bazy (te same co w Ingestorze)
+DB_HOST = "database"
+DB_NAME = "abcd_db"
+DB_USER = "admin"
+DB_PASSWORD = "admin_pass1234"
+
+def get_db_connection():
+    # Ta funkcja puka do drzwi bazy danych i otwiera połączenie
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+    return conn
--- esp32/include/secrets.h
+++ esp32/include/secrets.h
@@ -1,6 +1,9 @@
 #pragma once
-#define WIFI_SSID "esp_lab"
-#define WIFI_PASSWORD "12345678"
-#define MQTT_HOST "156.17.45.150"
+// #define WIFI_SSID "esp_lab"
+#define WIFI_SSID "NJU_Swiatlowod_4F80"
+// #define WIFI_PASSWORD "12345678"
+#define WIFI_PASSWORD "VJY33H5JLC35"
+// #define MQTT_HOST "156.17.45.150"
+#define MQTT_HOST "192.168.1.15"
 #define MQTT_PORT 1883
 #define MQTT_GROUP "g03"
